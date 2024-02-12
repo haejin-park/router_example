7ab09a6d3a87c43719065670426c4bdd85a5f9b2
haejin <luckyhaejin1126@gmail.com>
2024-02-12 19:07:47 +0900

    파라미터 값 읽어오기

--- src/page/ProductDetailpage.js
+++ src/page/ProductDetailpage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductDetailpage = () => {
+  const {id} = useParams();
+  console.log("id", id);
   return (
     <div>
-      <h1>Show Product Detail</h1>
+      <h1>Show Product Detail{id}</h1>
     </div>
   )
 }
