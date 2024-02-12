818ac67ab5333bb6eb799c1463028b79c8e19d03
haejin <luckyhaejin1126@gmail.com>
2024-02-12 18:49:42 +0900

    리액트 라우터(BrowserRouter, Routes, Route, Link, useNavigate, RESTful Routes)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Aboutpage from './page/Aboutpage';
+import Homepage from './page/Homepage';
+import ProductDetailpage from './page/ProductDetailpage';
+import Productpage from './page/Productpage';
+import {Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/about" element={<Aboutpage />}/>
+        <Route path="/products" element={<Productpage />}/>
+        <Route path="/products/:id" element={<ProductDetailpage />}/> 
+      </Routes>
     </div>
   );
 }
---
+++ src/page/ProductDetailpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetailpage = () => {
+  return (
+    <div>
+      <h1>Show Product Detail</h1>
+    </div>
+  )
+}
+
+export default ProductDetailpage
