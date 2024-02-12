c9e1713025f61589cd790a81b4dcc653de30fec6
haejin <luckyhaejin1126@gmail.com>
2024-02-12 19:43:39 +0900

    웹페이지를 보호하는법 redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Aboutpage from './page/Aboutpage';
 import Homepage from './page/Homepage';
+import Loginpage from './page/Loginpage';
 import ProductDetailpage from './page/ProductDetailpage';
 import Productpage from './page/Productpage';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate } from "react-router-dom";
+import Userpage from './page/Userpage';
 
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  const PrivateRoute = () => {
+    return authenticate === true? <Userpage /> : <Navigate to="/login"/>; 
+  }
   return (
     <div>
       <Routes>
@@ -14,6 +21,9 @@ function App() {
         <Route path="/about" element={<Aboutpage />}/>
         <Route path="/products" element={<Productpage />}/>
         <Route path="/products/:id" element={<ProductDetailpage />}/> 
+        <Route path="/login" element={<Loginpage />}/> 
+        <Route path="/user" element={<PrivateRoute />}/> 
+
       </Routes>
     </div>
   );
